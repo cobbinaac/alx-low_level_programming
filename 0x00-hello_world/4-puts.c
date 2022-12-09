9744e66fb41b9e408233e5b5076f91d06e8b693d
cobbinaac <acebuoyac@gmail.com>
2022-12-08 20:34:58 -0800

    first line of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /*
  * main - print a line of code using puts
  * 
- * Return: 0
+ * Return: O
 */
 int main(void)
 {
 	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+	return (O);
 }
