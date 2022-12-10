c93d73edf1087571448d8b172ad6b5d95b3564e2
cobbinaac <acebuoyac@gmail.com>
2022-12-09 21:27:47 -0800

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
-
- #include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - prints all possible combinations of two two-digit numbers
@@ -20,15 +19,27 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+				if (i !=
 
-		}
-	}
-	putchar('\n');
+
+#include <stdio.h>
+
+/**
+ * main - print the size of various types
+ *
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+
 	return (0);
 }
