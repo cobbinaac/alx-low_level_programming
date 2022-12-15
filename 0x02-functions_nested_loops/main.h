151d0866304a97bc58d3f83c1a0dfc395ac97fb8
cobbinaac <acebuoyac@gmail.com>
2022-12-14 16:12:47 -0800

    task1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,34 @@
-int _putchar(char c);
+#ifndef main_h
+	#define main_h
 
-void print_alphabet_x10(void);
 
-int _islower(int c);
+	int _putchar(char);
 
-int _isalpha(int c);
+	void print_alphabet(void);
 
-int _abs(int);
+	void print_alphabet_x10(void);
 
-int print_last_digit(int);
+	int _islower(int c);
 
-void jack_bauer(void);
+	int _isalpha(int c);
 
-void time_table(void);
+	int print_sign(int n);
 
-int add(int, int);
+	int _abs(int);
 
-void print_to_98(int n);
+	int print_last_digit(int);
 
-void print_times_table(int n);
+	void jack_bauer(void);
+
+	void times_table(void);
+
+	int add(int, int);
+
+	void print_to_98(int n);
+
+	void print_times_table(int n);
+
+
+	#endif
+
+
